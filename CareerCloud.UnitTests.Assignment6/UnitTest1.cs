67ec6f060bc93955168478dbfef2518b6516b38e
John Hinz <jhinz402@hotmail.com>
2017-10-05 12:37:24 -0400

    Add project files.

---
+++ CareerCloud.UnitTests.Assignment6/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CareerCloud.UnitTests.Assignment6
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
